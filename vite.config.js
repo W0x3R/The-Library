f4d1c89d31a35b02bdf150d62403c1fe3c140294
Stanislau Vyrski <vyr4376@yandex.ru>
2026-01-14 15:57:39 +0300

    feat: added a script for deploying the application

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
+  base: "/The-Library/",
   plugins: [cssInjectedByJsPlugin()],
   build: {
     outDir: "dist",
@@ -9,7 +10,6 @@ export default defineConfig({
     emptyOutDir: true,
     target: "es2015",
     sourcemap: false,
-    cssCodeSplit: false,
     rollupOptions: {
       output: {
         entryFileNames: "main.js",
